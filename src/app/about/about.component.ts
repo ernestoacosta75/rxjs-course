58b62a1efc853e05f0513f1f2892985b830126a5
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-08-26 17:49:36 +0200

    Observable concatenation.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -24,6 +24,13 @@ import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 export class AboutComponent implements OnInit {
 
     ngOnInit() {
+      const source1$ = of(1, 2, 3);
+      const source2$ = of(4, 5, 6);
+      const source3$ = of(7, 8, 9);
+
+      const result$ = concat(source1$, source2$, source3$);
+
+      result$.subscribe(console.log);
 
     }
 }
