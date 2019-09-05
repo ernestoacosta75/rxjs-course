83373c4579918290cb6f2ab84c218d340f528d35
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-09-05 18:00:43 +0200

    RxjJS - using merge operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -24,11 +24,10 @@ import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 export class AboutComponent implements OnInit {
 
     ngOnInit() {
-      const source1$ = of(1, 2, 3);
-      const source2$ = of(4, 5, 6);
-      const source3$ = of(7, 8, 9);
+      const interval1$ = interval(1000);
+      const interval2$ = interval1$.pipe(map(val => val * 10));
 
-      const result$ = concat(source1$, source2$, source3$);
+      const result$ = merge(interval1$, interval2$);
 
       result$.subscribe(console.log);
 
